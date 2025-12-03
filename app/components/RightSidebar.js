79f4704889c345cc328c14fb7397d7b8294a2b58
Maks Vintskovskyi <intadmirmaks@gmail.com>
2025-12-03 17:28:39 +0100

    suspence

--- app/components/RightSidebar.js
+++ app/components/RightSidebar.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import { Suspense } from 'react'
 import SearchBar from './SearchBar';
 
 const RightSidebar = () => {
   return (
     <div className='pl-8 pt-3'>
+      <Suspense>
         <div> <SearchBar placeholder="Search" /></div>
+      </Suspense>
     </div>
   )
 }
