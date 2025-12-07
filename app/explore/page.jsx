65e8d9511294c885aa1af326d9943446c64a9459
Maks <intadmirmaks@gmail.com>
2025-12-07 02:47:50 +0100

    suspence

--- app/explore/page.jsx
+++ app/explore/page.jsx
@@ -1,42 +1,10 @@
-"use client";
-export const dynamic = "force-dynamic";
-export const fetchCache = "force-no-store";
+import { Suspense } from "react";
+import SearchPage from "./SearchPage";
 
-import { useSearchParams } from "next/navigation";
-import {  useEffect, useState } from "react";
-import SearchBar from "../components/SearchBar";
-
-export default function SearchPage() {
-  const searchParams = useSearchParams();
-  const query = searchParams.get("q");
-  const [posts, setPosts] = useState([]);
-
-
-  useEffect(() => {
-    if (!query) return;
-    fetch(`/api/search?q=${query}`)
-      .then((res) => res.json())
-      .then((data) => setPosts(data));
-  }, [query]);
-
+export default function Page() {
   return (
-    <div className="w-full px-5 mx-auto p-4">
-
-        <div> <SearchBar placeholder="Search" /></div>
-
-
-      {posts.length === 0 && query && (
-        <p className="text-gray-500 mt-4">No results for "{query}"</p>
-      )}
-
-      <ul className="mt-4 space-y-4">
-        {posts.map((p) => (
-          <li key={p.id} className="border-b pb-2">
-            <h2 className="font-bold">{p.title}</h2>
-            <p className="text-gray-700">{p.body}</p>
-          </li>
-        ))}
-      </ul>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <SearchPage />
+    </Suspense>
   );
 }
